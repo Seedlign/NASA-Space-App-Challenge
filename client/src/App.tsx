11a6f554750fcd4f6922b1b7b30b37ece13a3d20
Seedlign <ricbarrios45@gmail.com>
2025-10-04 16:46:15 +0800

    non: msg change

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 const App: React.FC = () => {
   return (
-    <div className="min-h-screen bg-gray-100 p-4">
-      <h1 className="text-3xl font-bold text-center">NASA Space App Challenge</h1>
+    <div className="min-h-screen bg-gray-500 p-4">
+      <h1 className="text-3xl font-bold text-center">Yummy Master Jhezra YUmm!!</h1>
+      <h2 className="text-3xl font-bold text-center">ummmmm</h2>
+      <p className="text-3xl font-bold text-center">hihi</p>
       <div className="mt-4">
       </div>
     </div>
