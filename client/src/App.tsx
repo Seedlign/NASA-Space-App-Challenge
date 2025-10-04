de30eee48d4f7288cb69c91c3b5f3b5dcff590df
Seedlign <ricbarrios45@gmail.com>
2025-10-04 16:38:43 +0800

    initialization for front deps

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="min-h-screen bg-gray-100 p-4">
+      <h1 className="text-3xl font-bold text-center">NASA Space App Challenge</h1>
+      <div className="mt-4">
+      </div>
+    </div>
+  );
+};
+
+export default App;
